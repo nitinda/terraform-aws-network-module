711c719fe5a2e3be907522e57ff6acd2d126dc63
nitin-das <nitin.das@thomson.co.uk>
2019-09-17 17:16:36 +0100

    Corrected typo

--- internet-gateway.tf
+++ internet-gateway.tf
@@ -2,6 +2,6 @@ resource "aws_internet_gateway" "demo_internet_gateway" {
   vpc_id = "${aws_vpc.demo_vpc.id}"
 
   tags = "${merge(var.common_tags, map(
-    "Name", "terraform-demo-internet-gateway",z
+    "Name", "terraform-demo-internet-gateway",
   ))}"
 }
