e98b87b38e73c61722d1186ea1cafb4ea0664e43
nitin-das <nitin.das@thomson.co.uk>
2019-11-28 10:07:43 +0000

    Added new data

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_eip" "demo_epi" {
-  count = 2
+  count = "${length(var.private_subnets_cidr)}"
   vpc   = true
   
   tags = "${merge(var.common_tags, map(
@@ -8,7 +8,7 @@ resource "aws_eip" "demo_epi" {
 }
 
 resource "aws_nat_gateway" "demo_nat_gateway" {
-  count = 2
+  count = "${length(var.private_subnets_cidr)}"
   allocation_id = "${aws_eip.demo_epi.*.id[count.index]}"
   subnet_id     = "${aws_subnet.demo_subnet_public.*.id[count.index]}"
   depends_on    = ["aws_internet_gateway.demo_internet_gateway"]
