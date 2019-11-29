e4ffe7cd2056db6bc1a05909495490a675a403c0
nitin-das <nitin.das@thomson.co.uk>
2019-11-29 11:06:51 +0000

    Modified code

---
+++ vpc-route-db.tf
@@ -0,0 +1,15 @@
+### Database Route Table
+resource "aws_route_table" "demo_route_table_db" {
+    count  = "${length(var.db_subnets_cidr)}"
+    vpc_id = "${aws_vpc.demo_vpc.id}"
+    tags   = "${merge(var.common_tags, map(
+        "Name", "terraform-demo-route-table-db",
+    ))}"
+}
+
+### Database Route Table Associaion
+resource "aws_route_table_association" "demo_route_table_association_db" {
+  count          = "${length(var.db_subnets_cidr)}"
+  subnet_id      = "${element(aws_subnet.demo_subnet_db.*.id, count.index)}"
+  route_table_id = "${aws_route_table.demo_route_table_db.id}"
+}
---
+++ vpc-route-public.tf
@@ -0,0 +1,20 @@
+
+### Public Route Table
+resource "aws_route_table" "demo_route_table_public" {
+  count  = "${length(var.public_subnets_cidr)}"
+  vpc_id = "${aws_vpc.demo_vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.demo_internet_gateway.id}"
+  }
+  tags = "${merge(var.common_tags, map(
+    "Name", "terraform-demo-route-table-public",
+  ))}"
+}
+
+### Public Route Table Associaion
+resource "aws_route_table_association" "demo_route_table_association_public" {
+  count          = "${length(var.public_subnets_cidr)}"
+  subnet_id      = "${element(aws_subnet.demo_subnet_public.*.id, count.index)}"
+  route_table_id = "${aws_route_table.demo_route_table_public.id}"
+}
