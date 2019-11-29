f8ff1df0c919405dcbdb4d784e183dd437b4dccc
nitin-das <nitin.das@thomson.co.uk>
2019-11-29 14:10:46 +0000

    Modified Code

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-# DHCP Option
+# DHCP Option #####################################
 variable "domain_name_servers" {
   description = "List of name servers to configure in /etc/resolv.conf."
   type        = "list"
@@ -9,7 +9,7 @@ variable "domain_name" {
   type        = "list"
 }
 
-# VPC 
+# VPC ########################################
 variable "cidr_block" {
   description = "The CIDR block for the VPC."
 }
@@ -19,7 +19,7 @@ variable "enable_dns_hostnames" {
   default = "A boolean flag to enable/disable DNS hostnames in the VPC. Defaults false"
 }
 
-# Subnet
+# Subnet #####################################
 variable "public_subnets_cidr" {
   description = "The CIDR block for the subnet."
   type        = "list"
@@ -40,8 +40,12 @@ variable "availability_zones" {
   type        = "list"
 }
 
-# Tags
+# Tags ###################################
 variable common_tags {
   description = "Resources Tags"
   type = "map"
+}
+
+variable "tag_name_prefix" {
+  description = "Resources Name Tag prefix"
 }
