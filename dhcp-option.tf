70c677da18247226ea47d8eee141f4daabf2c63a
nitin-das <nitin.das@thomson.co.uk>
2019-11-29 14:05:50 +0000

    Modified Code

--- dhcp-option.tf
+++ dhcp-option.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_dhcp_options" "vpc_dhcp_options" {
-  domain_name_servers = "${var.domain_name_serverss}"
+  domain_name_servers = "${var.domain_name_servers}"
   domain_name         = "${var.domain_name}"
   tags                = "${merge(var.common_tags, map("Name", "${var.tag_name_prefix}-dhcp-options",))}"
 }
