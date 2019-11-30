a871e98f3b4a8196708046942fa190ed16721cb3
nitin-das <nitin.das@thomson.co.uk>
2019-11-30 23:54:49 +0000

    Added new code

--- internet-gateway.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_internet_gateway" "demo_internet_gateway" {
-  vpc_id = "${aws_vpc.demo_vpc.id}"
-  tags   = "${merge(var.common_tags, map("Name", "${var.tag_name_prefix}-internet-gateway",))}"
-}
