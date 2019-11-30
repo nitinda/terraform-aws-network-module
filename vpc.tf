aed25260a4387c67c26a1b37409229e6d3754b84
nitin-das <nitin.das@thomson.co.uk>
2019-11-30 23:58:00 +0000

    Added new code

---
+++ vpc.tf
@@ -0,0 +1,5 @@
+resource "aws_vpc" "demo_vpc" {
+  cidr_block           = "${var.cidr_block}"
+  enable_dns_hostnames = "${var.enable_dns_hostnames}"
+  tags                 = "${merge(var.common_tags, map("Name", "${var.tag_name_prefix}-vpc",))}"
+}
