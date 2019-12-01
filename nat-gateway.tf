444711970e7eb61eab25ac6e749a3176ad03549a
nitin-das <nitin.das@thomson.co.uk>
2019-12-01 00:08:36 +0000

    Added new code

---
+++ nat-gateway.tf
@@ -0,0 +1,13 @@
+resource "aws_eip" "demo_epi" {
+  count = "${length(var.private_subnets_cidr)}"
+  vpc   = true
+  tags  = "${merge(var.common_tags, map("Name", "${var.tag_name_prefix}-epi",))}"
+}
+
+resource "aws_nat_gateway" "demo_nat_gateway" {
+  count         = "${length(var.private_subnets_cidr)}"
+  allocation_id = "${aws_eip.demo_epi.*.id[count.index]}"
+  subnet_id     = "${aws_subnet.demo_subnet_public.*.id[count.index]}"
+  depends_on    = ["aws_internet_gateway.demo_internet_gateway"]
+  tags          = "${merge(var.common_tags, map("Name", "${var.tag_name_prefix}-nat-gateway-${count.index}",))}"
+}
