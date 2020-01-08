362fa247a9df496f3475731b6de648da3e26c95e
nitin-das <nitin.das@thomson.co.uk>
2020-01-08 14:03:50 +0000

    Modified code

--- dhcp-option.tf
+++ dhcp-option.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_dhcp_options" "vpc_dhcp_options" {
   domain_name_servers = var.domain_name_servers == [] ? ["AmazonProvidedDNS"] : var.domain_name_servers
-  domain_name         = var.domain_name == "" ? "${data.aws_region.name}.compute.internal" : var.domain_name
+  domain_name         = var.domain_name == "" ? "${data.aws_region.current.name}.compute.internal" : var.domain_name
   tags = merge(
     var.common_tags,
     {
